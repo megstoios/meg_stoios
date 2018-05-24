a4c20d58fb61dce44bc2aa07942ba38bc54a4d2d
megstoios <megstoios@github.com>
2018-05-24 23:19:16 +1000

    new3

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
 $(document).ready(function() {
 
-  $(function() {
-    $( ".draggable" ).draggable();
-    $(".resizable").resizable({
-      aspectRatio: true
-    });
-  });
+  var navicon = document.getElementById('navicon')
+  var navbar = document.getElementById('navbar')
+  var main = document.getElementById('main')
+  var acknowledgement = document.getElementById('acknowledgement')
+  var content = document.getElementById('content')
+  var left = document.getElementById('left')
+  var right = document.getElementById('right')
 
+  navicon.onclick =function(){
+      $('#navbar').slideToggle(1000);
+  }
+
+  acknowledgement.onmouseover = function(){
+    content.style.display = "block";
+  }
+
+  acknowledgement.onmouseout = function(){
+    content.style.display = "none";
+  }
+
+  navbar.onmouseleave = function(){
+    navbar.style.display = "none";
+  }
+
+  left.onclick = function(){
+    alert('lefty');
+  }
+
+  right.onclick = function(){
+    main.style.backgroundImage = "url('images/scan20.jpeg')"
+  }
 
 })
